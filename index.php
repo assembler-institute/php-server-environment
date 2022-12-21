576686a822430a5b283f7b6f4cb337307ec52433
Berta Garcia <bertagnavarro93@gmail.com>
2022-12-21 18:09:34 +0100

    server variables

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+$_SESSION['browser'] = $_SERVER['HTTP_USER_AGENT'];
+$_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
+$_SESSION['time'] = time();
+echo '<pre>';
+print_r($_SESSION);
+echo '</pre>';
+
+
+$inipath = php_ini_loaded_file();
+
+if ($inipath) {
+    echo 'Loaded php.ini: ' . $inipath;
+} else {
+    echo 'A php.ini file is not loaded';
+}
+
+
+?>
