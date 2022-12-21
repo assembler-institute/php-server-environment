03b4094a5276904f13b49e02669f6aa269fa44f3
monica-reverte <monicareverte24@gmail.com>
2022-12-21 13:39:19 +0100

    basicindex

---
+++ Session login Exercise/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action = "validate.php" method="POST">
+        <label>
+            Name:
+           <input type="text" name="name">
+        </label>
+
+        <br>
+
+        <label>
+            Password:
+            <input type="password" name="password">
+        </label>
+
+        <br>
+
+        <button type="submit">Log in</button>
+
+
+
+
+
+
+
+    </form>
+</body>
+</html>
