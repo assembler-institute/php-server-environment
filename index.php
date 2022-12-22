f0b1266da58de8a8b243a6544bff9507666ce46b
Miquel <abellagarciamiquel@gmail.com>
2022-12-22 11:31:01 +0100

    first version of project

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=MuseoModerno&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <?php
+    session_start();
+
+    if (isset($_SESSION['email'])) {
+        header("Location: panel.php");
+    }
+    ?>
+    <header>
+        <form id="login-form" action="validate.php" method="POST">
+            <label for="email">3mail</label>
+            <input required type="email" name="email" id="email">
+            <label for="password">P4ss</label>
+            <input required type="password" name="password" id="password">
+            <button type="submit">Enter</button>
+        </form>
+    </header>
+    <main id="login-section">
+        <h1 class="title">WORK SP4C3</h1>
+        <h2 class="subtitle">Do your magic</h2>
+        <?php
+        if (isset($_GET['error'])) {
+            echo "<div class='error'>
+            <p>INCORR3CT LOGIN</p>
+            </div>";
+        }
+        ?>
+    </main>
+</body>
+
+</html>
---
+++ panel.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panel</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=MuseoModerno&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php
+    session_start();
+
+
+    if (!isset($_SESSION['email'])) {
+        header('Location: index.php');
+    }
+    ?>
+    <main class="panel-main">
+        <header class="logout-form">
+          <p class="subtitle"> Hi, <?php echo $_SESSION['email'] ?> </p>
+            <form action="close_session.php">
+                <button id="logout-btn">Logout</button>
+            </form>
+        </header>
+        <h2 class="subtitle">Start a project</h2>
+        <section class="projects-container">
+            <div class="project">
+                <p>+</p>
+            </div>
+            <div class="project">
+                <p>+</p>
+            </div>
+            <div class="project">
+                <p>+</p>
+            </div>
+        </section>
+    </main>
+</body>
+
+</html>
