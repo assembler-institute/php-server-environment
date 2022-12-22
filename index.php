94ec98b646ebf12686fdb2b8e60cd2b7d58684c8
Isaura Martí <imarti.teixido@gmail.com>
2022-12-22 17:35:58 +0100

    css changes

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -15,21 +16,28 @@
     if (count($_SESSION)) {
         header("Location: panel.php");
     } else if (isset($_GET["error"])) {
-        echo "<div><h3>Enter a valid email and password!</h3></div>";
+        echo "<div class='error-invalid-user'><h3>Enter a valid email and password!</h3></div>";
     }
 
     ?>
-    <form action="validate.php" method="POST">
+    <div class="container-login">
+        <div class="img-login"></div>
+        <div class="container-form-login">
+            <form class="form-login" action="validate.php" method="POST">
 
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email">
+                <h2>Login</h2>
 
-        <label for="password">Pasword:</label>
-        <input type="password" name="password" id="password">
+                <label for="email">Email:</label>
+                <input type="email" name="email" id="email">
 
-        <button type="submit">Submit</button>
+                <label for="password">Pasword:</label>
+                <input type="password" name="password" id="password">
 
-    </form>
+                <button type="submit">Submit</button>
+
+            </form>
+        </div>
+    </div>
 </body>
 
 </html>
--- panel.php
+++ panel.php
@@ -6,9 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Page</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
-<body>
+<body class="body-panel-page">
 
     <?php
     session_start();
@@ -19,10 +20,12 @@
         echo "<button type='submit'>Logout</button>";
         echo "</form>";
     } else {
-        echo "It is a private section...</h2>";
+        echo "<div>";
+        echo "<h2>It is a private section...</h2>";
         echo "<form action='close_session.php' method='POST'>";
         echo "<button type='submit'>Return</button>";
         echo "</form>";
+        echo "</div>";
     }
     ?>
 
