4a64e079232a5294b7422e2d3a7961e87d87e71f
Miquel <abellagarciamiquel@gmail.com>
2022-12-22 11:41:28 +0100

    created dev branch

---
+++ modules/sessioncontrol.php
@@ -0,0 +1,19 @@
+<?php
+
+function loginUser()
+{
+    session_start();
+
+    $emaildb = 'm@m.com';
+    $passworddb = 'm';
+
+    $email = $_REQUEST['email'];
+    $password = $_REQUEST['password'];
+
+    if ($email === $emaildb && $password === $passworddb) {
+        $_SESSION['email'] = $email;
+        header('Location: panel.php');
+    } else {
+        header('Location: index.php?error=notvalidlogin');
+    }
+}
