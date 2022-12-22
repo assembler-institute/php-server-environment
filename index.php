c857fc473451f4bec76657a419b4ac3cdc38727e
Marian Roibu <marianroibu32@gmail.com>
2022-12-22 20:32:53 +0100

    fixed some bugs

--- index.php
+++ index.php
@@ -24,10 +24,10 @@
   
   </form>
   <?php
- if (isset($_SESSION['logged_in'])) {
-  session_destroy();
-  header('Location: index.php');
-}
+// if (isset($_SESSION['logged_in'])) {
+//   session_destroy();
+//   header('Location: index.php');
+// }
   
   ?>
 
--- panel.php
+++ panel.php
@@ -1,18 +1,40 @@
+<!DOCTYPE html
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="cyberpunk.css">
+    <script src="java.js" defer></script>
+    <title>CYBERPUNK</title>
+</head>
+<body>
+    <div class="scanline"></div>
+    <pre class="code"><span class="blink">█</span></pre>
+    <section>
+        <div class="hero-container">
+          <div class="environment"></div>
+          <a href='close_session.php' class="hero glitch layers" data-text="ログアウト"><span>ログアウト</span></a>
+        </div>
+      </section>
+    
+</body>
+</html>
+
+
+
 <?php
 
 session_start();
 if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {
   // Mostrar bienvenida y botón para cerrar sesión
-  header("Location: test.html");
+
   session_destroy();
 } else {
   // Mostrar mensaje de error
   header("Location: error.html" );
 }
 
-if (!isset($_SESSION['logged_in'])) {
-    header('Location: error.html');
-    exit;
-  }
+
 
 ?>
