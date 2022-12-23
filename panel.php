da9ba22e3f9ac61330ebf879409d59b8f403e8cf
Kriiiiiiiz <kriiiiiiiz@icloud.com>
2022-12-23 10:06:40 +0100

    Updated panel.php

--- panel.php
+++ panel.php
@@ -11,11 +11,18 @@ if (!isset($_SESSION["username"]))header("Location: index.php?zonaprivada");
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Panel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
+    <nav class="navbar bg-light">
+    <form class="container-fluid justify-content-start">
+        <button class="btn btn-outline-success me-2" type="button">Home</button>
+        <input class="btn btn-sm btn-outline-secondary" type="button" onclick="location.href='close_session.php';" value="Log Out" />
+    </form>
+    </nav>
     <h1>You are logged in !</h1>
-    <a href="close_session.php">Logout</a>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
