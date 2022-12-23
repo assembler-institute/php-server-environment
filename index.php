48d69ffa7ffefc3cb746e252a0e158109c0164af
Kriiiiiiiz <kriiiiiiiz@icloud.com>
2022-12-23 11:29:46 +0100

    Added title to index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Log In</title>
     <style>
     body  {
     background-image: url("assets/img/background.jpg");
