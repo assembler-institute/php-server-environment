bbf86a1998ec6b2999dc8294aa11e4f88d78a45b
Kriiiiiiiz <kriiiiiiiz@icloud.com>
2022-12-23 03:23:19 +0100

    Modified index.php

--- index.php
+++ index.php
@@ -4,23 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
+    <style>
+    body  {
+    background-image: url("assets/img/background.jpg");
+    background-color: #cccccc;
+    }
+    </style>
 </head>
 <body>
-    <?php
-        if(isset($_GET["error"]))echo $_GET["error"];
-        ?>
         <!-- Mejorar Error -->
-    <form action="validate.php" method="POST">
+    <form class="login" action="validate.php" method="POST">
         <label>
-            Username
-            <input type="text" name="username">
+            <input type="text" name="username" placeholder="Username">
         </label>
         <br>
         <label>
-            Password
-            <input type="password" name="password">
+            <input type="password" name="password" placeholder="Password">
         </label>
+        <?php
+            if(isset($_GET["error"]))
+                echo '<div class="warn">Incorrect username or password !</div>';
+            if(isset($_GET["zonaprivada"]))
+            echo '<div class="warn">Please log in !</div>'
+        ?>
         <br>
         <button type="submit">Login</button>
     </form>
